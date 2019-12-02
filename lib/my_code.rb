86dca65e0988b8cf2837d9dfd97a83c3e9b75a53
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-02 20:49:55 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0 
+  while i < array.length do 
+    new.push(yield(array[i]))
+    i -= -1 
+  end 
+  return new
+end 
+
+def reduce (array, starting_point = nil)
+ if !starting_point
+   total = array[0]
+   i = 1 
+ else 
+   total = starting_point
+   i = 0 
+ end 
+ while i < array.length do 
+   total = yield(total, array[i])
+   i -= -1
+ end 
+  total 
+ end
